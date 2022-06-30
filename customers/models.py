65c785d1e8f191a4c9a8f87e989b01953e4444eb
Heder Milani <hedermilani@hotmail.com>
2022-06-29 23:51:24 -0300

    Models e Migrate DB

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,16 @@
+from venv import create
 from django.db import models
 
 # Create your models here.
+
+class customers(models.Model):
+    first_name = models.CharField("First Name", max_length=255)
+    last_name = models.CharField("Last Name", max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.first_name
