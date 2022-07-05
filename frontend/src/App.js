a08ff6399c908d76558e754117d9e2e15af9eb1c
Heder Milani <hedermilani@hotmail.com>
2022-07-04 23:30:57 -0300

    Final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter} from "react-router-dom";
+import { Route, Link } from "react-router-dom";
+import CustomersList from "./CustomersList";
+import CustomerCreateUpdate from "./CustomerCreateUpdate";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const BaseLayout = () => (
+  <div className="container-flluid">
+    <nav className="nav navbar-expand-lg navbar-ligth bg-light">
+      <a className="navbar-brand" href="#">Django React Demo</a>
+      <button className="navbar-toggler" type="button" data-toggle="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link" href="/">CUSTOMERS</a>
+          <a className="nav-item nav-link" href="/customer">CREATE CUSTOMER</a>
+        </div>
+      </div>
+    </nav>
+    <div className="content">
+      <Route path="/" exact component={CustomersList}/>
+      <Route path="/customer/:pk" component={CustomerCreateUpdate}/>
+      <Route path="/customer/" component={CustomerCreateUpdate}/>
     </div>
-  );
+  </div>
+)
+
+class App extends Component {
+  render() {
+    return(
+      <BrowserRouter>
+        <BaseLayout/>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
